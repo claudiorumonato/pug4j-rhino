4005f6edc49e1f57146eceb858f41ce700b153a1
user <user@192.168.1.10>
2023-05-10 19:28:55 +0200

    PugModel

--- src/main/java/de/neuland/pug4j/model/PugModel.java
+++ src/main/java/de/neuland/pug4j/model/PugModel.java
@@ -6,10 +6,14 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 import de.neuland.pug4j.filter.Filter;
 import de.neuland.pug4j.parser.node.MixinNode;
@@ -19,13 +23,27 @@ public class PugModel implements Map<String, Object> {
 	private static final String LOCALS = "locals";
 	public static final String LOCAL_VARS = "pug4j__localVars";
 	public static final String PUG4J_MODEL_PREFIX = "pug4j__";
-	private Deque<Map<String, Object>> scopes = new LinkedList<Map<String, Object>>();
+	private Deque<Scriptable> scopes = new LinkedList<Scriptable>();
 	private Map<String, MixinNode> mixins = new HashMap<String, MixinNode>();
 	private Map<String, Filter> filter = new HashMap<String, Filter>();
 
+	private Context m_ctx;
+	private Scriptable m_scope;
+	private final boolean m_ownContext;
+	
 	public PugModel(Map<String, Object> defaults) {
+		this(defaults, null);
+	}
+	
+	public PugModel(Map<String, Object> defaults, Context ctx) {
+		
+		m_ctx = (m_ownContext = (ctx == null))
+				? Context.enter()
+				: ctx;
+		
+		m_scope = m_ctx.initStandardObjects();
+		
 		pushScope();
-
 		if (defaults != null) {
 			putAll(defaults);
 		}
@@ -33,9 +51,29 @@ public class PugModel implements Map<String, Object> {
 		putLocal(LOCALS, this);
 	}
 
+	public Scriptable scope() {
+		return scopes.getLast();
+	}
+
+	public Context context() {
+		return m_ctx;
+	}
+	
+	public Object parse(String expr, String name) {
+		Object res = null;
+		if (name == null)
+			name = "<anon>";
+		try {
+			res = m_ctx.evaluateString(m_scope, expr, name, 0, null);
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+		return res;
+	}
+
 	public void pushScope() {
-		HashMap<String, Object> scope = new HashMap<String, Object>();
-		scope.put(LOCAL_VARS, new HashSet<String>());
+		Scriptable scope = m_ctx.newObject(m_scope);
+		scope.put(LOCAL_VARS, scope, m_ctx.newObject(m_scope));
 		scopes.add(scope);
 	}
 
@@ -51,17 +89,29 @@ public class PugModel implements Map<String, Object> {
 		return mixins.get(name);
 	}
 
+	public void clear(boolean dispose) {
+		scopes.clear();
+		m_scope = null;
+		if (m_ownContext) {
+			Context.exit();
+			m_ctx = (!dispose) ? Context.enter() : null;
+		}
+		if (!dispose) {
+			m_scope = m_ctx.initStandardObjects();
+			scopes.add(m_ctx.newObject(m_scope));
+		}
+	}
+	
 	@Override
 	public void clear() {
-		scopes.clear();
-		scopes.add(new HashMap<String, Object>());
+		clear(false);
 	}
-
+	
 	@Override
 	public boolean containsKey(Object key) {
-		for (Iterator<Map<String, Object>> i = scopes.descendingIterator(); i.hasNext();) {
-			Map<String, Object> scope = i.next();
-			if (scope.containsKey(key)) {
+		for (Iterator<Scriptable> i = scopes.descendingIterator(); i.hasNext();) {
+			Scriptable scope = i.next();
+			if (scope.has(key.toString(), scope)) {
 				return true;
 			}
 		}
@@ -69,10 +119,10 @@ public class PugModel implements Map<String, Object> {
 	}
 
 	public boolean knowsKey(Object key) {
-		for (Iterator<Map<String, Object>> i = scopes.descendingIterator(); i.hasNext();) {
-			Map<String, Object> scope = i.next();
-			Set<String> localVars = (Set<String>)scope.get(LOCAL_VARS);
-			if (scope.containsKey(key) || localVars.contains(key)) {
+		for (Iterator<Scriptable> i = scopes.descendingIterator(); i.hasNext();) {
+			Scriptable scope = i.next();
+			Set<String> localVars = getKeys((Scriptable)scope.get(LOCAL_VARS, scope));
+			if (scope.has(key.toString(), scope) || localVars.contains(key)) {
 				return true;
 			}
 		}
@@ -81,19 +131,21 @@ public class PugModel implements Map<String, Object> {
 
 	@Override
 	public boolean containsValue(Object value) {
-		for (Iterator<Map<String, Object>> i = scopes.descendingIterator(); i.hasNext();) {
-			Map<String, Object> scope = i.next();
-			if (scope.containsValue(value)) {
-				return true;
+		for (Iterator<Scriptable> i = scopes.descendingIterator(); i.hasNext();) {
+			Scriptable scope = i.next();
+			Set<String> keys = getKeys(scope);
+			for (String key: keys) {
+				if (scope.get(key, scope).equals(value))
+					return true;
 			}
 		}
 		return false;
 	}
 
 	@Override
-	public Set<java.util.Map.Entry<String, Object>> entrySet() {
+	public Set<Map.Entry<String, Object>> entrySet() {
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		for (String key : keySet()) {
+		for (String key: keySet()) {
 			map.put(key, get(key));
 		}
 		return map.entrySet();
@@ -102,19 +154,19 @@ public class PugModel implements Map<String, Object> {
 	@Override
 	// adds the object to the highest scope
 	public Object get(Object key) {
-		for (Iterator<Map<String, Object>> i = scopes.descendingIterator(); i.hasNext();) {
-			Map<String, Object> scope = i.next();
-			if (scope.containsKey(key)) {
-				return scope.get(key);
+		for (Iterator<Scriptable> i = scopes.descendingIterator(); i.hasNext();) {
+			Scriptable scope = i.next();
+			if (scope.has(key.toString(), scope)) {
+				return scope.get(key.toString(), scope);
 			}
 		}
 		return null;
 	}
-	private Map<String,Object> getScopeWithKey(Object key) {
-		for (Iterator<Map<String, Object>> i = scopes.descendingIterator(); i.hasNext();) {
-			Map<String, Object> scope = i.next();
-			Set<String> localVars = (Set<String>)scope.get(LOCAL_VARS);
-			if (scope.containsKey(key) || localVars.contains(key)) {
+	private Scriptable getScopeWithKey(Object key) {
+		for (Iterator<Scriptable> i = scopes.descendingIterator(); i.hasNext();) {
+			Scriptable scope = i.next();
+			Set<String> localVars = getKeys((Scriptable)scope.get(LOCAL_VARS, scope));
+			if (scope.has(key.toString(), scope) || localVars.contains(key)) {
 				return scope;
 			}
 		}
@@ -125,12 +177,20 @@ public class PugModel implements Map<String, Object> {
 		return keySet().isEmpty();
 	}
 
+	private Set<String> getKeys(Scriptable scope) {
+		Set<String> keys = new LinkedHashSet<String>();
+		for (Object id: scope.getIds())
+			if (id instanceof String)
+				keys.add(id.toString());
+		return keys;
+	}
+	
 	@Override
 	// returns a set of unique keys
 	public Set<String> keySet() {
 		Set<String> keys = new HashSet<String>();
-		for (Iterator<Map<String, Object>> i = scopes.descendingIterator(); i.hasNext();) {
-			keys.addAll(i.next().keySet());
+		for (Iterator<Scriptable> i = scopes.descendingIterator(); i.hasNext();) {
+			keys.addAll(getKeys(i.next()));
 		}
 		return keys;
 	}
@@ -147,41 +207,44 @@ public class PugModel implements Map<String, Object> {
 	}
 
 	private Set<String> getLocalVars() {
-		return (Set<String>) scopes.getLast().get(LOCAL_VARS);
+		Scriptable scope = scopes.getLast();
+		return getKeys((Scriptable) scope.get(LOCAL_VARS, scope));
 	}
 
 	// adds the object to the current scope
 	public Object putLocal(String key, Object value) {
 		Object currentValue = get(key);
-		Map<String, Object>	scope = scopes.getLast();
-		scope.put(key, value);
+		Scriptable	scope = scopes.getLast();
+		scope.put(key, scope, value);
 		return currentValue;
 	}
 
 	// adds the object to the scope where the variable was last defined
 	public Object putGlobal(String key, Object value) {
 		Object currentValue = get(key);
-		Map<String, Object> scope = getScopeWithKey(key);
+		Scriptable scope = getScopeWithKey(key);
 		if (scope == null)
 			scope = scopes.getLast();
-		scope.put(key, value);
+		scope.put(key, scope, value);
 		return currentValue;
 	}
 
 	@Override
 	// addes all map entries to the current scope map
 	public void putAll(Map<? extends String, ? extends Object> m) {
-		scopes.getLast().putAll(m);
+		Scriptable scope = scopes.getLast();
+		for (String key: m.keySet())
+			scope.put(key, scope, m.get(key));
 	}
 
 	@Override
 	// removes the scopes first object with the given key
 	public Object remove(Object key) {
-		for (Iterator<Map<String, Object>> i = scopes.descendingIterator(); i.hasNext();) {
-			Map<String, Object> scope = i.next();
-			if (scope.containsKey(key)) {
-				Object object = scope.get(key);
-				scope.remove(key);
+		for (Iterator<Scriptable> i = scopes.descendingIterator(); i.hasNext();) {
+			Scriptable scope = i.next();
+			if (scope.has(key.toString(), scope)) {
+				Object object = scope.get(key.toString(), scope);
+				scope.delete(key.toString());
 				return object;
 			}
 		}
